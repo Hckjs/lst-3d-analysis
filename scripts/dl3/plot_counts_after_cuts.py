29310aa07ef05e8c0298e6e4348a9ea5ffeaef5f
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2023-09-15 15:55:26 +0200

    Add logging code

--- scripts/dl3/plot_counts_after_cuts.py
+++ scripts/dl3/plot_counts_after_cuts.py
@@ -1,7 +1,12 @@
+import logging
 from argparse import ArgumentParser
 
 from astropy.table import Table
 from matplotlib import pyplot as plt
+
+from scriptutils.log import setup_logging
+
+log = logging.getLogger(__name__)
 
 
 def main(input_path, output_path):
@@ -41,6 +46,8 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-i", "--input-path", required=True)
     parser.add_argument("-o", "--output-path", required=True)
+    parser.add_argument("--log-file")
+    parser.add_argument("-v", "--verbose", action="store_true")
     args = parser.parse_args()
-
+    setup_logging(logfile=args.log_file, verbose=args.verbose)
     main(args.input_path, args.output_path)
