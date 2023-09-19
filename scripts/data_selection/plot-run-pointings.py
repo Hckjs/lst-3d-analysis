05e753d6762f221bcbae0ed16366a4899bde0201
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2023-09-19 14:34:52 +0200

    Plot cos(zen) and az

--- scripts/data_selection/plot-run-pointings.py
+++ scripts/data_selection/plot-run-pointings.py
@@ -1,21 +1,54 @@
 import logging
 from argparse import ArgumentParser
 
+import astropy.units as u
+import matplotlib
+import numpy as np
 from astropy.table import Table
 from matplotlib import pyplot as plt
 
 from scriptutils.log import setup_logging
 
+if matplotlib.get_backend() == "pgf":
+    from matplotlib.backends.backend_pgf import PdfPages
+else:
+    from matplotlib.backends.backend_pdf import PdfPages
+
+
 log = logging.getLogger(__name__)
 
 
-def main():
-    pointings = Table.read(args.input_path)
+def main(input_path, output_path):
+    pointings = Table.read(input_path)
+    figures = []
 
-    # TODO 2 plots (also azimuth)
     fig, ax = plt.subplots()
-    ax.plot(pointings["run_id"], pointings["zen"], ls="")
-    fig.savefig(args.output_path)
+    ax.plot(pointings["run_id"], pointings["zen"].quantity.to_value(u.rad), ls="")
+    ax.set_xlabel("Run Id")
+    ax.set_ylabel("Zenith / deg")
+    figures.append(fig)
+
+    ax.plot(
+        pointings["run_id"],
+        np.cos(pointings["zen"].quantity.to_value(u.rad)),
+        ls="",
+    )
+    ax.set_xlabel("Run Id")
+    ax.set_ylabel("Cos (zenith)")
+    figures.append(fig)
+
+    fig, ax = plt.subplots()
+    ax.plot(pointings["run_id"], pointings["az"].quantity.to_value(u.rad), ls="")
+    ax.set_xlabel("Run Id")
+    ax.set_ylabel("Azimuth / deg")
+    figures.append(fig)
+
+    if output_path is None:
+        plt.show()
+    else:
+        with PdfPages(output_path) as pdf:
+            for fig in figures:
+                pdf.savefig(fig)
 
 
 if __name__ == "__main__":
@@ -27,4 +60,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     setup_logging(logfile=args.log_file, verbose=args.verbose)
-    main()
+    main(args.input_path, args.output_path)
