Fix cos calculation

scripts/dl3/plot_rates.py
@@ -71,7 +71,7 @@ def main(input_path, output):
         figures.append(fig)
 
         fig, ax = plt.subplots()
-        ax.scatter(np.cos(zen), r)
+        ax.scatter(np.cos(np.deg2rad(zen)), r)
         ax.set_xlabel("cos(zd)")
         ax.set_ylabel(f"Rate / {rate.unit}")
         figures.append(fig)